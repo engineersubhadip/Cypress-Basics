3ed8ba094124888e8318e01a99b4cb05d9d6a0de
unknown <subhadipdas911@gmail.com>
2026-07-05 21:26:13 +0530

    cy.contains() implementation

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-  viewportWidth: 1280,
-  viewportHeight: 720,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
     baseUrl: "https://playground.bondaracademy.com/",
     specPattern: "cypress/e2e/**/*.{cy.js,cy.jsx,cy.ts,cy.tsx,js}",
--- cypress/e2e/feature/intro.js
+++ cypress/e2e/feature/intro.js
@@ -2,10 +2,20 @@
 
 beforeEach("Open test app", () => {
   cy.visit("/");
+  cy.contains('a[title="Forms"]', "Forms").click();
+  cy.contains('a[title="Form Layouts"]', "Form Layouts").click();
 });
 
-afterEach("Closing the app", () => {
-  cy.reload();
-});
+// afterEach("Closing the app", () => {
+//   cy.reload();
+// });
 
-it("Hello World 1", () => {});
+it("Cypress Locators", () => {
+  // get() -> To find elements on the page globally
+  // find() -> To find child elements
+  // contains() -> To find elements using text [It will only find the first match on the page, unlike get() which will find all the matches on the page]
+  // contains() -> It is case sensitive, it is looking for exact match in the DOM
+  cy.contains('button[status="primary"]', "Sign in");
+  cy.contains("nb-card", "Horizontal form").find("button");
+  cy.contains("nb-card", "Horizontal form").contains("Sign in");
+});
