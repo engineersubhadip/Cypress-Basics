d05eefd4e44fcadd2234c6ab3b36814f4f283bff
unknown <subhadipdas911@gmail.com>
2026-07-07 12:00:33 +0530

    Addressed 1 more point related to contains()

--- cypress/e2e/feature/intro.js
+++ cypress/e2e/feature/intro.js
@@ -12,10 +12,21 @@ beforeEach("Open test app", () => {
 
 it("Cypress Locators", () => {
   // get() -> To find elements on the page globally
-  // find() -> To find child elements
+  // find() -> To find child elements [Parents can be many, find() will target the parent having the reqd locator EX : cy.get("input") -> This is can return multiple elements, .find(".radio") -> This will find the 1st child having this class inside input]
   // contains() -> To find elements using text [It will only find the first match on the page, unlike get() which will find all the matches on the page]
   // contains() -> It is case sensitive, it is looking for exact match in the DOM
+  // contains() -> It can also be used to find child elements by its text
+
   cy.contains('button[status="primary"]', "Sign in");
   cy.contains("nb-card", "Horizontal form").find("button");
   cy.contains("nb-card", "Horizontal form").contains("Sign in");
+});
+
+it.only("Child Elements", () => {
+  // * Approach 1
+  cy.contains("nb-card", "Using the Grid").find(".row").find("button");
+  // * Approach 2
+  cy.contains("nb-card", "Using the Grid").contains(".row", "Sign in");
+  // * Lets target "Option 1"
+  cy.get("nb-card").find("nb-radio-group").contains("Option 1");
 });
